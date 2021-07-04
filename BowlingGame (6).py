fb3909ac5ca21837221fbaaf5979aa4fd466ccf8
sir306 <hardingnick00@gmail.com>
2021-07-05 00:21:40 +1200

    Have fixed syntax errors that were found such as line spaces incorrect. While doing this the list storing the throws was renamed to throws due to the conflict between the variable and the method below when being called. assertEquals was changed to assertEqual due to depreciation. Code is now working and runnable

--- BowlingGame (6).py
+++ BowlingGame (6).py
@@ -1,26 +1,28 @@
 import unittest
+
+
 class BowlingGame(object):
     def __init__(self):
-        self.throw= []
-        self.score= 0
+        self.throws = []
+        self.score = 0
 
-    def throw(self,pins):
-        self.throw.append(pins)
-
+    def throw(self, pins):
+        self.throws.append(pins)
 
     def calculate_score(self):
         ball = 0
         for frames in range(10):
-            if self.throws[ball]==10:
-                self.score +=10 + self.throws[ball+1] + self.throws[ball +2]
+            if self.throws[ball] == 10:
+                self.score += 10 + self.throws[ball + 1] + self.throws[ball + 2]
                 ball += 1
-            elif self.throw[ball] + self.throws[ball+1] == 10:
-                self.score += 10 + self.throws [ball +2]
-                ball +=2
-            else:
-                self.score += self.throws[ball] + self.throw[ball + 1]
+            elif self.throws[ball] + self.throws[ball + 1] == 10:
+                self.score += 10 + self.throws[ball + 2]
                 ball += 2
- 
+            else:
+                self.score += self.throws[ball] + self.throws[ball + 1]
+                ball += 2
+
+
 class BowlingGameTests(unittest.TestCase):
     def throw_many(self, game, number_of_times, pins):
         for _ in range(number_of_times):
@@ -28,27 +30,25 @@ class BowlingGameTests(unittest.TestCase):
 
     def test_all_gutters(self):
         game = BowlingGame()
-        self.throw_many(game, 20 ,0 )
+        self.throw_many(game, 20, 0)
         game.calculate_score()
-        self.assertEquals(game.score,0)
+        self.assertEqual(game.score, 0)
 
     def test_perfect_game(self):
         game = BowlingGame()
         self.throw_many(game, 12, 10)
         game.calculate_score()
-        self.assertEquals(game.score, 300)
+        self.assertEqual(game.score, 300)
 
-        def test_all_ones(self):
-            game = BowlingGame()
-            number_of_times = 20
-            pins = 1
-            self.throw_many(game, number_of_times, pins)
-            game.calculate_score()
-            self.assertEquals(game.score, 20)
+    def test_all_ones(self):
+        game = BowlingGame()
+        number_of_times = 20
+        pins = 1
+        self.throw_many(game, number_of_times, pins)
+        game.calculate_score()
+        self.assertEqual(game.score, 20)
 
-
-
-    def test_different_throws (self):
+    def test_different_throws(self):
         game = BowlingGame()
         game.throw(6)
         game.throw(0)
@@ -58,8 +58,7 @@ class BowlingGameTests(unittest.TestCase):
         for _ in range(15):
             game.throw(0)
         game.calculate_score()
-        self.assertEquals(game.score, 15)
-
+        self.assertEqual(game.score, 15)
 
     def test_for_spare(self):
         game = BowlingGame()
@@ -70,13 +69,13 @@ class BowlingGameTests(unittest.TestCase):
         for _ in range(16):
             game.throw(0)
         game.calculate_score()
-        self.assertEquals(game.score, 24)
+        self.assertEqual(game.score, 24)
 
     def test_for_strike(self):
-        game=BowlingGame()
+        game = BowlingGame()
         game.throw(10)
         game.throw(4)
         game.throw(2)
-        self.throw_many(game, 17,0)
+        self.throw_many(game, 17, 0)
         game.calculate_score()
-        self.assertEquals(game.score, 22)
+        self.assertEqual(game.score, 22)
